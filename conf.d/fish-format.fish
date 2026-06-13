28025d6432d8d8eacf9f87000073f90c4d7aed80
Drazape <Drazape@outlook.com>
2026-06-13 17:51:38 +0530

    add Fisher support

---
+++ conf.d/fish-format.fish
@@ -0,0 +1,7 @@
+function _fish-format_install --on-event=fish-format_install --description='Install dependency: fish-helpText'
+    fisher install Drazape/fish-helpText
+end
+
+function _fish-format_update --on-event=fish-format_update --description='Update dependency: fish-helpText'
+    fisher update Drazape/fish-helpText
+end
