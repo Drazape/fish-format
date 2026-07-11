aacbd855b6548f4c7cfbe982d4056033faa751a0
Drazape <Drazape@outlook.com>
2026-07-11 15:52:07 +0530

    install hooks: remove
    Dependency install hooks create a loop since the system installation is done through help-text

--- conf.d/fish-format.fish
+++
@@ -1,7 +0,0 @@
-function _fish-format_install --on-event=fish-format_install --description='Install dependency: fish-helpText'
-    fisher install Drazape/fish-helpText
-end
-
-function _fish-format_update --on-event=fish-format_update --description='Update dependency: fish-helpText'
-    fisher update Drazape/fish-helpText
-end
