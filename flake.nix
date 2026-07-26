c28122e60118c430de657f1a9196f68afdf27cde
Drazape <Drazape@outlook.com>
2026-07-26 17:18:00 +0530

    chore(indentation): Flake: spaces → tabs

--- flake.nix
+++ flake.nix
@@ -1,27 +1,28 @@
 {
-  description = "ANSI formatting for Fish";
+	description = "ANSI formatting for Fish";
 
-  inputs = {
-    flake-parts = { type="github"; owner="hercules-ci"; repo="flake-parts"; };
-    nixpkgs = { type="github"; owner="NixOS"; repo="nixpkgs"; ref="nixpkgs-unstable"; };
-  };
+	inputs = {
+		flake-parts = { type="github"; owner="hercules-ci"; repo="flake-parts"; };
+		nixpkgs = { type="github"; owner="NixOS"; repo="nixpkgs"; ref="nixpkgs-unstable"; };
+	};
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        packages = let pkgName = "fish-format"; in {
-          default = self'.packages.${pkgName};
-          ${pkgName} = builtins.derivation {
-            name = pkgName;
-            inherit system;
-            builder = pkgs.lib.getExe pkgs.fish;
-            args = [ "--no-config" "--private" "--" ./install.fish ./. ];
+	outputs = inputs@{ flake-parts, ... }:
+		flake-parts.lib.mkFlake { inherit inputs; } {
+			systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+			perSystem = { config, self', inputs', pkgs, system, ... }: {
+				packages = let pkgName = "fish-format"; in {
+					default = self'.packages.${pkgName};
+					${pkgName} = builtins.derivation {
+						name = pkgName;
+						inherit system;
 
-            HOME = "/tmp/"; # Temporary home directory to write history to
-            PATH = pkgs.coreutils+"/bin";
-          };
-        };
-      };
-    };
+						builder = pkgs.lib.getExe pkgs.fish;
+						args = [ "--no-config" "--private" "--" ./install.fish ./. ];
+
+						HOME = "/tmp/"; # Temporary home directory to write history to
+						PATH = pkgs.coreutils+"/bin";
+					};
+				};
+			};
+		};
 }
