64a592fd0321e48c698ca83c5f392a07aed90635
Drazape <Drazape@outlook.com>
2026-08-30 16:45:20 +0530

    style(install): function name parsing: new lines
    Put the string parsing to compute the function name into separate lines for readability

--- install.fish
+++ install.fish
@@ -31,7 +31,9 @@ function install-components --description='Install a component of the program' -
     for source in "$root_dir"{$source_dir}/**.fish
         set --local -- output_path {$source} # Same output path in case of no root directory
         set --query --local -- root_dir && set --local -- output_path (string split --fields=2 --max=1 -- "$root_dir" {$source}) # Remove root directory from the output path
-        install -D --mode=644 -- {$source} "$out"/"$remote"share/fish/vendor_{$vendor_dir}.d/(string split --fields=2 --max=1 -- {$source_dir} {$output_path})
+        install -D --mode=644 -- {$source} "$out"/"$remote"share/fish/vendor_{$vendor_dir}.d/(
+            string split --fields=2 --max=1 -- {$source_dir} {$output_path}
+        )
     end
 end
 
